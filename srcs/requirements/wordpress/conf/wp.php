a822d693f03fd3f3ff2db5cdd281d5f406d23eb3
gclement42 <clement.games125@gmail.com>
2023-12-12 16:44:19 +0100

    ajout de fichier .php

---
+++ srcs/requirements/wordpress/conf/wp.php
@@ -0,0 +1,11 @@
+/** The name of the database for WordPress */
+define( 'DB_NAME', 'database' );
+
+/** Database username */
+define( 'DB_USER', 'errr' );
+
+/** Database password */
+define( 'DB_PASSWORD', ${SQL_PASSWORD} );
+
+/** Database hostname */
+define( 'DB_HOST', 'localhost' );
